5f2d59d808292e1fce1a859d4e90a8112775798a
Michael Morrell <mjmorrel@uci.edu>
2015-09-05 22:23:08 -0700

    testing intellij commit3

---
+++ hello2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func tester(){
+	fmt.Printf("Hello, World!\n")
+	fmt.Printf("Hello from Windows, too!\n")
+
+	var b, c int = 1, 2
+	fmt.Println(b, c, 3, "\n")
+
+	fmt.Println("And now git ssh works! Cleann!\n")
+}
