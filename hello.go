be8ca871ab31045fc152d958281a10e20afeacfb
Michael Morrell <mjmorrel@uci.edu>
2015-09-05 21:42:39 -0700

    testing

--- hello.go
+++ hello.go
@@ -4,5 +4,8 @@ import "fmt"
 
 func main(){
 	fmt.Printf("Hello, World!\n")
-	fmt.Printf("Hello from Windows, too!")
+	fmt.Printf("Hello from Windows, too!\n")
+
+	var b, c int = 1, 2
+	fmt.Println(b, c, 3, "\n")
 }
